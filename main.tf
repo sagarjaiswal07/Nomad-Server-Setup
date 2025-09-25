b13376bcb40d4f5c01ff8d8b0de437d57aa92415
Sagar Jaiswal <105929549+sagarjaiswal07@users.noreply.github.com>
2025-09-25 18:23:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,12 +19,12 @@ module "compute" {
   client_nodes_count = var.client_nodes_count
   vpc_id             = module.networking.vpc_id
 
-  # Pass the outputs from the security module INTO the compute module
+ 
   bastion_sg_id      = module.security.bastion_sg_id
   nomad_server_sg_id = module.security.nomad_server_sg_id
   nomad_client_sg_id = module.security.nomad_client_sg_id
   alb_sg_id          = module.security.alb_sg_id
 
-  # Explicit dependency to ensure networking and security are fully up
+
   depends_on = [module.networking, module.security]
 }
